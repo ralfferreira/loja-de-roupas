b49949fc4746c5532a42357e248a46f28e1c5510
Ralf <ralfferreira995@gmail.com>
2023-05-04 23:01:12 -0300

    Add destructor in Acessorios

---
+++ Acessorios.cpp
@@ -0,0 +1,37 @@
+#include "Acessorios.h"
+
+Acessorios::Acessorios() : Produto(){}
+
+Acessorios::Acessorios(string nomeProduto, double valorDoProduto, string tamanho, string cor, string tipo) : Produto(nomeProduto, valorDoProduto), tamanho(tamanho), cor(cor), tipo(tipo){
+  this->tamanho = tamanho;
+  this->cor = cor;
+  this->tipo = tipo;
+}
+
+Acessorios::~Acessorios(){}
+
+string Acessorios::getTamanho(){
+  return tamanho;
+}
+string Acessorios::getCor(){
+  return cor;
+}
+string Acessorios::getTipo(){
+  return tipo;
+}
+void Acessorios::setTamanho(string tamanho){
+  this->tamanho = tamanho;
+}
+void Acessorios::setCor(string cor){
+  this->cor = cor;
+}
+void Acessorios::setTipo(string tipo){
+    this->tipo = tipo;
+}
+
+void Acessorios::MostrarNaTela(){
+  Produto::MostrarNaTela();
+  cout << "Tamanho do acessorio: " << tamanho << endl;
+  cout << "Cor do acessorio: " << cor << endl;
+  cout << "Material do acessorio: " << tipo << endl;
+}
